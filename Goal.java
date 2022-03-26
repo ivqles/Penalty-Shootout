c018d1a4b437a0fdc478d08f914dcf0fa7443b6b
Ivy Le <102192526+ivqles@users.noreply.github.com>
2022-03-25 18:50:24 -0700

    Added Goal Class with Structures for Some Methods

---
+++ Goal.java
@@ -0,0 +1,22 @@
+public class Goal {
+
+    // Instantiating Variables
+
+
+    // Constructing Goal / Game Board (3 x 5)
+    public Goal() {
+    }
+
+    // toString Method for Proper String Output
+    public String toString() {
+        return "";
+    }
+
+    // called during every round; sets randomized place for "G"
+    public void setGoalie() {
+    }
+    
+    public int kick(int row, int col) {
+        return 0; // Dummy return; will return different numbers based on win/loss
+    }
+}
