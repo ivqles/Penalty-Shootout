9d25cc96ff24fe77ad73fa1d7a51a4fbd756aad4
TheBucketHatv <102395728+TheBucketHatv@users.noreply.github.com>
2022-03-26 10:55:17 -0700

    added point system and made sure user input is within bounds

--- Goal.java
+++ Goal.java
@@ -1,22 +1,60 @@
 public class Goal {
 
-    // Instantiating Variables
+     // Instantiating Variables
+     static char emptySpace = 'O';
+     static char ball = 'B';
+     static char goalie = 'G';
+     static int countRows = 3;
+     static int countColumns = 5;
+     static int score = 0;
+     public int playerPos = 0;
+     public int goaliePos = 0;
+     char[][] goalArray = new char[countRows][countColumns];
+ 
+     // Constructing Goal / Game Board (3 x 5)
+     public Goal() {
+         for (int row = 0; row < countRows; row++) {
+             for (int col = 0; col < countColumns; col++) {
+                 goalArray[row][col] = emptySpace;
+             }
+         }
+     }
+ 
+     // toString Method for Proper String Output
+     public String toString() {
+         String goalArrayString = "";
+         for (char[] row : goalArray) {
+             for (char placeholder : row) {
+                 goalArrayString += placeholder + " ";
+             }
+             goalArrayString += "\n";
+         }
+         return goalArrayString;
+     }
+ 
+     // called during every round; sets randomized place for "K"
+     public void setGoalie() {
+         int col =  (int)(Math.random()*(countColumns - 1));
+         int row =  (int)(Math.random()*(countRows - 1));  
+         goalArray[row][col] = goalie;
+         goaliePos = row*3 + col*5;
+     }
+ 
+     //sets the ball to position provided by the player
+     public void kick(int row, int col) {
+         goalArray[row][col] = ball;
+         playerPos = row*3 + col*5;
+          
+     }
 
-
-    // Constructing Goal / Game Board (3 x 5)
-    public Goal() {
-    }
-
-    // toString Method for Proper String Output
-    public String toString() {
-        return "";
-    }
-
-    // called during every round; sets randomized place for "G"
-    public void setGoalie() {
-    }
-    
-    public int kick(int row, int col) {
-        return 0; // Dummy return; will return different numbers based on win/loss
-    }
+     public int totalScore(){
+         if(goaliePos!=playerPos){
+             score++;
+         }
+         return score;
+     }
+ 
+     public String getOutcome() {
+         return "";
+     }
 }
--- Main.java
+++ Main.java
@@ -1,29 +1,50 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String args[]){
+        Scanner x = new Scanner(System.in);
         Goal game = new Goal();
 
         //introduction
-       System.out.println("Welcome to the ARENA!");
-       System.out.println("Your task: shoot a goal! Choose the spot where the goalie can't reach!");
-    
-       while(game.totalScore()<3) {
+        System.out.println("Welcome to the ARENA!");
+        System.out.println("Your task: shoot a goal! Choose the spot where the goalie can't reach!");
+        int score = game.totalScore();
 
-       game.reset(); //returns a 3 by 5 grid
-        System.out.print("Enter the row number:");
-        Scanner x = new Scanner(System.in);
-        int r = x.nextInt();
-        System.out.print("\n" + "Enter the column number:");
-        int c = x.nextInt();
+        while(score < 3) {
+            System.out.println(game); //returns a 3 by 5 grid
+            System.out.print("Enter the row number:");
+            int r = x.nextInt() - 1;
+            while(r<0 || r>2){
+                System.out.println("That is not a valid number. Try again!");
+                r = x.nextInt()-1;
+            }
+            System.out.print("\n" + "Enter the column number:");
+            int c = x.nextInt() - 1;
+            while(c<0 || c>4){
+                System.out.println("That is not a valid number. Try again!");
+                c = x.nextInt();
+            }
+        
+            game.setGoalie(); //goalie position is placed randomly on the grid
+            game.kick(r,c); //inputs client's position on the grid
+            System.out.println(game); //returns grid positions
+            game.getOutcome(); //returns a random statement depending on the results 
+            score = game.totalScore();
+            System.out.println("You have earned " + score + " points!"); //returns total score
+            if (score != 3) {
+                System.out.println("\n Next round!");
+            }
+        } 
+       x.close(); //closes Scanner
 
-        game.setGoalie(); //goalie position is placed randomly on the grid
-        game.kick(r,c); //inputs client's position on the grid
-        game.result(); //returns grid positions
-        game.outcome(); //returns a random statement depending on the results 
-        System.out.println("You have earned " + game.totalScore() + " points!"); //returns total score
-       } 
-      
        //if there are four modes...     easy / med / hard / abstract (1, 2, 3, 4)
        //medium would have increasing goalie positions by 1 //hard would be the user can't choose the same spot twice //abstract would be a changing goal size
+       /*
+       Goal game2 = new Goal();
+       System.out.println("Choose your mode:" + game2.introduction()); //return all the modes and their descriptions
+       Scanner i =new Scanner(System.in);
+       int diff = i.readInt();
+       game2.setLevel(diff);
+       */
+    
     }
 }
